9225706df0aa9db5272d311063c3942066d77b3d
Jesper Dangaard Brouer <brouer@redhat.com>
2014-10-22 15:05:27 +0200

    time_bench_sample: Example of benchmarking execution time
    Sample measures different sync primitives the kernel uses often.
    
    The output is avail in the kernel log.
    
    Notice "per elem" cost indicate that e.g. for locking the cost
    result is actually (lock+unlock)/2.  This comes from I've used
    this framework for measuring queues that allow bulking of
    elements, and I wanted to measure the cost per element.
    
    Example output on a CPU E5-2630:
    
    time_bench: Type:lock_unlock Per elem: 19 cycles(tsc) 8.689 ns (step:0)
     - (measurement period time:1.737825818 sec time_interval:1737825818)
     - (invoke count:200000000 tsc_interval:3996996753)
    
    time_bench: Type:local_bh Per elem: 8 cycles(tsc) 3.870 ns (step:0)
     - (measurement period time:0.774018176 sec time_interval:774018176)
     - (invoke count:200000000 tsc_interval:1780240549)
    
    time_bench: Type:local_irq Per elem: 3 cycles(tsc) 1.452 ns (step:0)
     - (measurement period time:0.290556556 sec time_interval:290556556)
     - (invoke count:200000000 tsc_interval:668268412)
    
    time_bench: Type:local_irq_save Per elem: 17 cycles(tsc) 7.805 ns (step:0)
     - (measurement period time:1.561088120 sec time_interval:1561088120)
     - (invoke count:200000000 tsc_interval:3590500626)
    
    time_bench: Type:preempt Per elem: 5 cycles(tsc) 2.364 ns (step:0)
     - (measurement period time:0.472965394 sec time_interval:472965394)
     - (invoke count:200000000 tsc_interval:1087819451)
    
    time_bench: Type:call_func Per elem: 5 cycles(tsc) 2.178 ns (step:0)
     - (measurement period time:0.217866832 sec time_interval:217866832)
     - (invoke count:100000000 tsc_interval:501093036)
    
    time_bench: Type:call_func_ptr Per elem: 5 cycles(tsc) 2.530 ns (step:0)
     - (measurement period time:0.253031536 sec time_interval:253031536)
     - (invoke count:100000000 tsc_interval:581971841)
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

---
+++ kernel/lib/time_bench_sample.c
@@ -0,0 +1,192 @@
+/*
+ * Sample: Benchmarking code execution time inside the kernel
+ *
+ */
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/module.h>
+#include <linux/time.h>
+#include <linux/time_bench.h>
+#include <linux/spinlock.h>
+
+static int verbose=1;
+
+static DEFINE_SPINLOCK(my_lock);
+static int time_lock_unlock(
+	struct time_bench_record *rec, void *data)
+{
+	int i;
+	uint64_t loops_cnt = 0;
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+		spin_lock(&my_lock);
+		loops_cnt++;
+		barrier();
+		spin_unlock(&my_lock);
+		loops_cnt++;
+	}
+	time_bench_stop(rec, loops_cnt);
+	return loops_cnt;
+}
+
+static int time_local_bh(
+	struct time_bench_record *rec, void *data)
+{
+	int i;
+	uint64_t loops_cnt = 0;
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+		local_bh_disable();
+		loops_cnt++;
+		barrier();
+		local_bh_enable();
+		loops_cnt++;
+	}
+	time_bench_stop(rec, loops_cnt);
+	return loops_cnt;
+}
+
+static int time_local_irq(
+	struct time_bench_record *rec, void *data)
+{
+	int i;
+	uint64_t loops_cnt = 0;
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+		local_irq_disable();
+		loops_cnt++;
+		barrier();
+		local_irq_enable();
+		loops_cnt++;
+	}
+	time_bench_stop(rec, loops_cnt);
+	return loops_cnt;
+}
+
+static int time_local_irq_save(
+	struct time_bench_record *rec, void *data)
+{
+	int i;
+	uint64_t loops_cnt = 0;
+	unsigned long flags;
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+		local_irq_save(flags);
+		loops_cnt++;
+		barrier();
+		local_irq_restore(flags);
+		loops_cnt++;
+	}
+	time_bench_stop(rec, loops_cnt);
+	return loops_cnt;
+}
+
+static int time_preempt(
+	struct time_bench_record *rec, void *data)
+{
+	int i;
+	uint64_t loops_cnt = 0;
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+		preempt_disable();
+		loops_cnt++;
+		barrier();
+		preempt_enable();
+		loops_cnt++;
+	}
+	time_bench_stop(rec, loops_cnt);
+	return loops_cnt;
+}
+
+static void noinline measured_function(volatile int *var)
+{
+	(*var) = 1;
+}
+static int time_func(
+	struct time_bench_record *rec, void *data)
+{
+	int i, tmp;
+	uint64_t loops_cnt = 0;
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+		measured_function(&tmp);
+		loops_cnt++;
+	}
+	time_bench_stop(rec, loops_cnt);
+	return loops_cnt;
+}
+
+struct func_ptr_ops {
+	void (*func)(volatile int *var);
+	unsigned int (*func2)(unsigned int count);
+};
+static struct func_ptr_ops my_func_ptr __read_mostly = {
+	.func  = measured_function,
+	//.func2 = ring_queue_fake_test,
+};
+static int time_func_ptr(
+	struct time_bench_record *rec, void *data)
+{
+	int i, tmp;
+	uint64_t loops_cnt = 0;
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+		my_func_ptr.func(&tmp);
+		loops_cnt++;
+	}
+	time_bench_stop(rec, loops_cnt);
+	return loops_cnt;
+}
+
+
+int run_timing_tests(void)
+{
+	uint32_t loops = 100000000;
+
+	time_bench_loop(loops, 0, "lock_unlock", NULL, time_lock_unlock);
+	time_bench_loop(loops, 0, "local_bh", NULL, time_local_bh);
+	time_bench_loop(loops, 0, "local_irq", NULL, time_local_irq);
+	time_bench_loop(loops, 0, "local_irq_save", NULL, time_local_irq_save);
+	time_bench_loop(loops, 0, "preempt", NULL, time_preempt);
+	time_bench_loop(loops, 0, "call_func", NULL, time_func);
+	time_bench_loop(loops, 0, "call_func_ptr", NULL, time_func_ptr);
+	return 0;
+}
+
+static int __init time_bench_sample_module_init(void)
+{
+	if (verbose)
+		pr_info("Loaded\n");
+
+	if (run_timing_tests() < 0) {
+		return -ECANCELED;
+	}
+
+	return 0;
+}
+module_init(time_bench_sample_module_init);
+
+static void __exit time_bench_sample_module_exit(void)
+{
+	if (verbose)
+		pr_info("Unloaded\n");
+}
+module_exit(time_bench_sample_module_exit);
+
+MODULE_DESCRIPTION("Sample: Benchmarking code execution time in kernel");
+MODULE_AUTHOR("Jesper Dangaard Brouer <netoptimizer@brouer.com>");
+MODULE_LICENSE("GPL");
