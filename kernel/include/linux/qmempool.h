f37df6b99232ec1860fe6a1086a362439fadcb80
Jesper Dangaard Brouer <brouer@redhat.com>
2014-10-29 13:11:56 +0100

    mm: qmempool: a quick queue based mempool
    A quick or queue-based memory pool, that functions as a cache
    in-front of kmem_cache SLAB/SLUB allocators.
    
    A quick queue-based memory pool, that functions as a cache in-front
    of kmem_cache SLAB/SLUB allocators.  Which allows faster than
    SLAB/SLUB reuse/caching of fixed size memory elements
    
    The speed gain comes from, the shared storage, using a Lock-Free
    queue that supports bulking elements with a single cmpxchg.
    
    The Lock-Free queue is based on an array (of pointer to elements).
    This make access more cache optimal, as e.g. on 64bit 8 pointers
    can be stored per cache-line (which is superior to a linked list
    approach).
    
    Only storing the pointers to elements, is also beneficial as we
    don't touch the elements data.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

---
+++ kernel/include/linux/qmempool.h
@@ -0,0 +1,311 @@
+/*
+ * qmempool - a quick queue based mempool
+ *
+ * A quick queue-based memory pool, that functions as a cache in-front
+ * of kmem_cache SLAB/SLUB allocators.  Which allows faster than
+ * SLAB/SLUB reuse/caching of fixed size memory elements
+ *
+ * The speed gain comes from, the shared storage, using a Lock-Free
+ * queue that supports bulking elements with a single cmpxchg.
+ *
+ * The Lock-Free queue is based on an array (of pointer to elements).
+ * This make access more cache optimal, as e.g. on 64bit 8 pointers
+ * can be stored per cache-line (which is superior to a linked list
+ * approach).  Only storing the pointers to elements, is also
+ * beneficial as we don't touch the elements data.
+ *
+ * Copyright (C) 2014, Red Hat, Inc., Jesper Dangaard Brouer
+ *  for licensing details see kernel-base/COPYING
+ */
+
+#ifndef _LINUX_QMEMPOOL_H
+#define _LINUX_QMEMPOOL_H
+
+#include <linux/ring_queue.h>
+#include <linux/prefetch.h>
+
+/* Bulking is an essential part of the performance gains as this
+ * amortize the cost of cmpxchg ops used when accessing sharedq
+ */
+#define QMEMPOOL_BULK 16
+#define QMEMPOOL_REFILL_MULTIPLIER 2
+
+struct qmempool_percpu {
+	struct ring_queue *localq;
+	/* room for percpu stats */
+	uint64_t refill_cnt;
+	uint64_t full_cnt; /* push back (to shared) elements when full */
+	int owner_cpu;
+};
+
+struct qmempool {
+	/* The shared queue (sharedq) is a Multi-Producer-Multi-Consumer
+	 *  queue where access is protected by an atomic cmpxchg operation.
+	 *  The queue support bulk transfers, which amortize the cost
+	 *  of the atomic cmpxchg operation.
+	 */
+	struct ring_queue	*sharedq;
+
+	/* Per CPU local "cache" queues for faster lockfree access.
+	 * The local queues (localq) are Single-Producer-Single-Consumer
+	 * queues as they are per CPU.
+	 */
+	//struct ring_queue __percpu *localq[NR_CPUS];
+	//FIXME: do dynamic percpu alloc
+	// Example see: commit b7779d06f995
+	// functions: alloc_percpu() per_cpu_ptr()
+	struct qmempool_percpu __percpu *percpu;
+
+	/* Backed by some SLAB kmem_cache */
+	struct kmem_cache	*kmem;
+
+	/* Setup */
+// Size settings are redundant as this is recorded in the ring_queue
+//	uint32_t	localq_sz; /* Size of local (per CPU) queue */
+//	uint32_t	sharedq_sz;/* Size of shared queue */
+	// some thresh for when to refill
+	// param/flag for requesting prefetching
+	uint32_t prealloc;
+	gfp_t gfp_mask;
+};
+
+extern void qmempool_destroy(struct qmempool *pool);
+extern struct qmempool* qmempool_create(
+	uint32_t localq_sz, uint32_t sharedq_sz, uint32_t prealloc,
+	struct kmem_cache *kmem, gfp_t gfp_mask);
+
+extern void* __qmempool_alloc_from_sharedq(
+	struct qmempool *pool, gfp_t gfp_mask, struct ring_queue *localq);
+extern void* __qmempool_alloc_from_slab(struct qmempool *pool, gfp_t gfp_mask);
+extern bool __qmempool_free_to_slab(struct qmempool *pool, void **elems);
+
+//#define DEBUG_PERCPU 1
+static inline void debug_percpu(struct qmempool_percpu *cpu)
+{
+#ifdef DEBUG_PERCPU
+	if (unlikely(cpu->owner_cpu == -1)) {
+		cpu->owner_cpu = smp_processor_id();
+		return;
+	}
+	if (cpu->owner_cpu != smp_processor_id()) {
+		WARN_ON(1);
+		pr_err("BUG localq changed CPU %d -> %d\n",
+		       cpu->owner_cpu, smp_processor_id());
+	}
+#endif /* DEBUG_PERCPU */
+}
+
+/* This is needed to protect access to the percpu variables (SPSC
+ * queues), e.g. avoiding the process gets rescheduled on another CPU.
+ *
+ * IDEA: When used from softirq, take advantage of the protection
+ * softirq gives.  A softirq will never preempt another softirq,
+ * running on the same CPU.  The only event that can preempt a softirq
+ * is an interrupt handler (and perhaps we don't need to support
+ * calling qmempool from an interrupt).  Another softirq, even the
+ * same one, can run on another CPU however, but these helpers are
+ * only protecting our percpu variables.
+ *
+ * Thus, our percpu variables are safe if current the CPU is the one
+ * serving the softirq (tested via in_serving_softirq()), like:
+ *
+ *  if (!in_serving_softirq())
+ *		local_bh_disable();
+ *
+ * Q: BUT that if CONFIG_PREEMPT is enabled?!? Can it preempt a softirq?
+ *
+ */
+static inline void __qmempool_preempt_disable(void)
+{
+	/* 4.291 ns cost for preempt_{disable,enable} with
+	 *  CONFIG_PREEMPT is not set, but CONFIG_PREEMPT_COUNT=y
+	 */
+	 preempt_disable();
+//	if (!in_serving_softirq())
+//		local_bh_disable(); //7.459 ns cost local_bh_{disable,enable}
+
+	/* If we know/assure that this cannot be called with IRQ's
+	 * disabled, then we use the more lightweight
+	 * local_irq_{disable,enable} 2.860 ns cost instead of
+	 * local_irq_save()+local_irq_restore() 14.840 ns cost
+	 */
+	/* Cannot be used from interrupt context */
+//	BUG_ON(in_interrupt());
+}
+
+static inline void __qmempool_preempt_enable(void)
+{
+	preempt_enable();
+//	if (!in_serving_softirq())
+//		local_bh_enable();
+
+//	local_irq_enable();
+}
+
+/* Elements - alloc and free functions are inlined here for
+ * performance reasons, as the per CPU lockless access should be as
+ * fast as possible.
+ */
+
+static __always_inline void *
+__qmempool_alloc_node(struct qmempool *pool, gfp_t gfp_mask, int node)
+{
+	//TODO: handle numa node stuff e.g. numa_mem_id()
+	void *element;
+	struct qmempool_percpu *cpu;
+	int res;
+
+	__qmempool_preempt_disable();
+
+	/* 1. attempt get element from local per CPU queue */
+	cpu = this_cpu_ptr(pool->percpu);
+	res = ring_queue_sc_dequeue(cpu->localq, (void **)&element);
+	if (res == 0) {/* success: element dequeues from localq */
+		//prefetchw(element); // TODO: test effect
+		__qmempool_preempt_enable();
+		return element;
+	}
+
+	/* 2. attempt get element from shared queue.  This involves
+	 * refilling the localq for next round.
+	 *
+	 * Q: Do we need to "keep" preempt/local_xxx_disable()
+	 * A: Yes, probably because we will refill localq
+	 * Problem: this way we only support GFP_ATOMIC...
+	 */
+	element = __qmempool_alloc_from_sharedq(pool, gfp_mask, cpu->localq);
+	if (element) {
+		__qmempool_preempt_enable();
+		return element;
+	}
+	__qmempool_preempt_enable();
+
+	/* 3. handle if sharedq runs out of elements (element == NULL)
+	 * slab can can sleep if (gfp_mask & __GFP_WAIT), thus must
+	 * not run with preemption disabled.
+	 */
+	element = __qmempool_alloc_from_slab(pool, gfp_mask);
+	return element;
+}
+
+static inline void* __qmempool_alloc(struct qmempool *pool, gfp_t gfp_mask)
+{
+	return __qmempool_alloc_node(pool, gfp_mask, -1);
+}
+
+
+/* Defined in header, because this allow us to change
+ * __qmempool_preempt_enable() compile time, depending on usage.
+ *
+ * MUST be called with __qmempool_preempt_disable()
+ *
+ * Noinlined because it uses a lot of stack.
+ */
+static noinline_for_stack bool
+__qmempool_free_to_sharedq(struct qmempool *pool, struct ring_queue *localq)
+{
+	void *elems[QMEMPOOL_BULK]; /* on stack variable */
+	int res;
+
+	/* This function is called when the localq is full. Thus,
+	 * elements from localq needs to be returned to sharedq (or if
+	 * shared is full, need to be free'ed for real)
+	 */
+
+	/* Make room in localq */
+	res = ring_queue_sc_dequeue_bulk(localq, elems, QMEMPOOL_BULK);
+	// should be "_burst" else localq must minimum be 32
+	if (unlikely(res < 0))
+		goto failed;
+
+        /* Successful dequeue from localq */
+	/* Place elems in sharedq */
+	res = ring_queue_mp_enqueue_bulk(pool->sharedq,
+					 elems, QMEMPOOL_BULK);
+	if (res == 0) /* Success enqueued to sharedq */
+		return true;
+
+	/* Allow slab kmem_cache_free() to run with preemption */
+	__qmempool_preempt_enable();
+	__qmempool_free_to_slab(pool, elems);
+	return false;
+failed:
+	/* dequeing from a full localq should always be possible */
+	BUG();
+	return false;
+}
+
+static inline void __qmempool_free(struct qmempool *pool, void *elem)
+{
+	struct qmempool_percpu *cpu;
+	int res;
+	bool used_sharedq;
+
+	__qmempool_preempt_disable();
+
+	/* 1. attempt to free/return element to local per CPU queue */
+	cpu = this_cpu_ptr(pool->percpu);
+	debug_percpu(cpu);
+	res = ring_queue_sp_enqueue(cpu->localq, elem);
+	if (res == 0) /* success: element enqueued to localq */
+		goto done;
+
+	/* IDEA: use ring_queue watermark feature, to allow enqueue of
+	 * this cache-hot elem, and then return some to sharedq.
+	 */
+
+	/* 2. localq cannot store more elements, need to return some
+	 * from localq to sharedq, to make room.
+	 */
+	used_sharedq = __qmempool_free_to_sharedq(pool, cpu->localq);
+	if (!used_sharedq) {
+		pr_err("%s() used free_to_slab localq:%d sharedq:%d"
+		       " irqs_disabled:%d in_softirq:%lu cpu:%d\n",
+		       __func__, ring_queue_count(cpu->localq),
+		       ring_queue_count(pool->sharedq), irqs_disabled(),
+		       in_softirq(), smp_processor_id());
+		__qmempool_preempt_disable();
+		//cpu = this_cpu_ptr(pool->percpu); // have to reload "cpu" ptr
+	}
+
+	/* 3. this elem is more cache hot, keep it in localq */
+	debug_percpu(cpu);
+	cpu = this_cpu_ptr(pool->percpu); // have to reload "cpu" ptr
+	debug_percpu(cpu);
+	res = ring_queue_sp_enqueue(cpu->localq, elem);
+	if (unlikely(res < 0)) { /* should have been be room in localq!?! */
+		WARN_ON(1);
+		pr_err("%s() Why could this happen? localq:%d sharedq:%d"
+		       " irqs_disabled:%d in_softirq:%lu cpu:%d\n",
+		       __func__, ring_queue_count(cpu->localq),
+		       ring_queue_count(pool->sharedq), irqs_disabled(),
+		       in_softirq(), smp_processor_id());
+		kmem_cache_free(pool->kmem, elem);
+	}
+done:
+	__qmempool_preempt_enable();
+}
+
+/* Debug hack to ease profiling functions when not inlined
+ */
+//#define QMEMPOOL_DEBUG_PROFILING 1
+#ifdef QMEMPOOL_DEBUG_PROFILING
+extern void* qmempool_alloc(struct qmempool *pool, gfp_t gfp_mask);
+extern void* qmempool_alloc_node(struct qmempool *pool, gfp_t gfp_mask, int node);
+extern void qmempool_free(struct qmempool *pool, void *elem);
+#else /* !QMEMPOOL_DEBUG_PROFILING */
+static inline void* qmempool_alloc(struct qmempool *pool, gfp_t gfp_mask)
+{
+	return __qmempool_alloc_node(pool, gfp_mask, -1);
+}
+static inline void* qmempool_alloc_node(struct qmempool *pool, gfp_t gfp_mask, int node)
+{
+	return __qmempool_alloc_node(pool, gfp_mask, -1);
+}
+static inline void qmempool_free(struct qmempool *pool, void *elem)
+{
+	return __qmempool_free(pool, elem);
+}
+#endif /* QMEMPOOL_DEBUG_PROFILING */
+
+#endif /* _LINUX_QMEMPOOL_H */
---
+++ kernel/mm/qmempool.c
@@ -0,0 +1,314 @@
+/*
+ * qmempool - a quick queue based mempool
+ *
+ * Copyright (C) 2014, Red Hat, Inc., Jesper Dangaard Brouer
+ *  for licensing details see kernel-base/COPYING
+ */
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/module.h>
+#include <linux/mm.h>
+#include <linux/slab.h>
+#include <linux/export.h>
+#include <linux/percpu.h>
+#include <linux/qmempool.h>
+
+/* Global settings */
+
+/* Due to hotplug CPU support, we need access to all qmempools
+ * in-order to cleanup elements in localq for the CPU going offline.
+ *
+ * FIXME: NOT IMPLEMENTED this is just a prototype
+#ifdef CONFIG_HOTPLUG_CPU
+static LIST_HEAD(qmempool_list);
+static DEFINE_SPINLOCK(qmempool_list_lock);
+#endif
+ */
+
+/* True if x is a power of 2 */
+#define POWEROF2(x) (((x) & ((x) - 1)) == 0)
+
+/* Creating and destroying the qmempool
+ */
+
+void qmempool_destroy(struct qmempool *pool)
+{
+	void *elem = NULL;
+	int j;
+
+	if (pool->percpu) {
+		for_each_possible_cpu(j) {
+			struct qmempool_percpu *cpu =
+				per_cpu_ptr(pool->percpu, j);
+
+			while (ring_queue_dequeue(cpu->localq,
+						  (void **)&elem) >= 0)
+				kmem_cache_free(pool->kmem, elem);
+			BUG_ON(!ring_queue_empty(cpu->localq));
+			ring_queue_free(cpu->localq);
+		}
+		free_percpu(pool->percpu);
+	}
+
+	if (pool->sharedq) {
+		while (ring_queue_dequeue(pool->sharedq, (void **)&elem) >= 0)
+			kmem_cache_free(pool->kmem, elem);
+		BUG_ON(!ring_queue_empty(pool->sharedq));
+		ring_queue_free(pool->sharedq);
+	}
+
+	kfree(pool);
+}
+EXPORT_SYMBOL(qmempool_destroy);
+
+struct qmempool*
+qmempool_create(uint32_t localq_sz, uint32_t sharedq_sz, uint32_t prealloc,
+		struct kmem_cache *kmem, gfp_t gfp_mask)
+{
+	struct qmempool* pool;
+	int i, j, res;
+	void *elem;
+
+	/* Validate constraints, e.g. due to bulking */
+	if (localq_sz < QMEMPOOL_BULK) {
+		/* FIXME: perhaps <= because ring_queue 16 can only
+		 * contain 15 elems */
+		pr_err("%s() localq size(%d) too small for bulking\n",
+		       __func__, localq_sz);
+		return NULL;
+	}
+	if (sharedq_sz <= (QMEMPOOL_BULK * QMEMPOOL_REFILL_MULTIPLIER)) {
+		/* Minimum sharedq size is 64 due to refill and return
+		 * bulking needs sufficient space.  Due to ring_queue
+		 * implementation size can only contain is its size
+		 * minus 1, thus size 32 can only contain 31 elems
+		 * which is to small for REFILL_MULTIPLIER(2) * BULK(16)
+		 */
+		pr_err("%s() sharedq size(%d) too small for bulk refill\n",
+		       __func__, sharedq_sz);
+		return NULL;
+	}
+	if (!POWEROF2(localq_sz) || !POWEROF2(sharedq_sz)) {
+		pr_err("%s() queue sizes (%d/%d) must be power-of-2\n",
+		       __func__, localq_sz, sharedq_sz);
+		return NULL;
+	}
+	if (prealloc >= sharedq_sz) {
+		/* ring_queue limit is its size minus 1 */
+		pr_err("%s() prealloc(%d) req >= sharedq size(%d)\n",
+		       __func__, prealloc, sharedq_sz);
+		return NULL;
+	}
+	if ((prealloc % QMEMPOOL_BULK) != 0) {
+		pr_warn("%s() prealloc(%d) should be div by BULK size(%d)\n",
+			__func__, prealloc, QMEMPOOL_BULK);
+	}
+	if (!kmem) {
+		pr_err("%s() kmem_cache is a NULL ptr\n",  __func__);
+		return NULL;
+	}
+
+	pool = kzalloc(sizeof(*pool), gfp_mask);
+	if (!pool)
+		return NULL;
+	pool->kmem     = kmem;
+	pool->gfp_mask = gfp_mask;
+
+	/* MPMC (Multi-Producer-Multi-Consumer) ring queue */
+	pool->sharedq = ring_queue_create(sharedq_sz, 0);
+	if (pool->sharedq == NULL) {
+		pr_err("%s() failed to create shared queue(%d)\n",
+		       __func__, sharedq_sz);
+		qmempool_destroy(pool);
+		return NULL;
+	}
+
+	pool->prealloc = prealloc;
+	for (i = 0; i < prealloc; i++) {
+		elem = kmem_cache_alloc(pool->kmem, gfp_mask);
+		if (!elem) {
+			pr_err("%s() kmem_cache out of memory?!\n",  __func__);
+			qmempool_destroy(pool);
+			return NULL;
+		}
+		res = ring_queue_enqueue(pool->sharedq, elem);
+		BUG_ON(res < 0);
+	}
+
+	pool->percpu = alloc_percpu(struct qmempool_percpu);
+	if (pool->percpu == NULL) {
+		pr_err("%s() failed to alloc percpu\n", __func__);
+		qmempool_destroy(pool);
+		return NULL;
+	}
+
+	/* SPSC (Single-Consumer-Single-Producer) ring queue per CPU */
+	for_each_possible_cpu(j) {
+		struct qmempool_percpu *cpu = per_cpu_ptr(pool->percpu, j);
+
+		cpu->localq = ring_queue_create(localq_sz,
+						RING_F_SP_ENQ|RING_F_SC_DEQ);
+		if (cpu->localq == NULL) {
+			pr_err("%s() failed alloc localq on cpu:%d\n",
+			       __func__, j);
+			qmempool_destroy(pool);
+			return NULL;
+		}
+		cpu->refill_cnt = 0;
+		cpu->full_cnt = 0;
+		cpu->owner_cpu = -1;
+	}
+//	pool->localq_sz = localq_sz; // redundant
+
+	return pool;
+}
+EXPORT_SYMBOL(qmempool_create);
+
+/* element handling
+ */
+
+/* Debug hack to ease profiling functions when not inlined */
+#ifdef QMEMPOOL_DEBUG_PROFILING
+noinline void* qmempool_alloc(struct qmempool *pool, gfp_t gfp_mask)
+{
+       return __qmempool_alloc_node(pool, gfp_mask, 0);
+}
+EXPORT_SYMBOL(qmempool_alloc);
+
+noinline void* qmempool_alloc_node(struct qmempool *pool, gfp_t gfp_mask, int node)
+{
+       return __qmempool_alloc_node(pool, gfp_mask, 0);
+}
+EXPORT_SYMBOL(qmempool_alloc_node);
+
+noinline void qmempool_free(struct qmempool *pool, void *elem)
+{
+	return __qmempool_free(pool, elem);
+}
+EXPORT_SYMBOL(qmempool_free);
+#endif
+
+/* Assumed called with local_bh_disable() or preempt_disable() */
+void * __qmempool_alloc_from_sharedq(struct qmempool *pool, gfp_t gfp_mask,
+				   struct ring_queue *localq)
+{
+	void *elems[QMEMPOOL_BULK]; /* on stack variable */
+	void *elem;
+	int res;
+
+	/* This function is called when the localq runs out-of
+	 * elements.  Thus, the localq needs refilling */
+
+	/* Costs atomic "cmpxchg", but amortize cost by bulk dequeue */
+	res = ring_queue_mc_dequeue_bulk(pool->sharedq, elems, QMEMPOOL_BULK);
+	// FIXME: should be "_burst" variant if e.g. only 15 elements is left
+	if (likely(res == 0)) {/* Success */
+		/* FIXME: Consider prefetching data part of elements
+		 * here, it should be an optimal place to hide memory
+		 * prefetching.  Especially given the localq is known
+		 * to be an empty FIFO which guarantees the order objs
+		 * are accessed in.
+		 */
+		elem = elems[0]; /* extract one element */
+		/* Refill localq */
+		res = ring_queue_sp_enqueue_bulk(localq,
+						 &elems[1],
+						 QMEMPOOL_BULK-1);
+		/* FIXME: localq should be empty, thus enqueue should
+		 * succeed, but races can exists. This MUST be fixed,
+		 * for now add an ASSERT, then users will notice this
+		 * problem need handling/implemeting the hard-way ;-)
+		 */
+		BUG_ON(res < 0);
+
+		return elem;
+	}
+	return NULL;
+}
+EXPORT_SYMBOL(__qmempool_alloc_from_sharedq);
+
+void * __qmempool_alloc_from_slab(struct qmempool *pool, gfp_t gfp_mask)
+{
+	void *elems[QMEMPOOL_BULK]; /* on stack variable */
+	void *elem;
+	int res, i, j;
+	/* This function is called when sharedq runs-out of elements.
+	 * Thus, sharedq needs to be refilled from slab.
+	 */
+
+	/* TODO: Consider prefetching this elem */
+	elem = kmem_cache_alloc(pool->kmem, gfp_mask);
+	/* Note: This one extra alloc will "unalign" the
+	 * number of elements in localq from BULK(16) setting.
+	 */
+	if (elem == NULL) {
+		/* slab is depleted, no reason to call below allocs */
+		pr_err("%s() slab is depleted returning NULL\n", __func__);
+		WARN_ON(!elem);
+		return NULL;
+	}
+
+	for (i = 0; i < QMEMPOOL_REFILL_MULTIPLIER; i++) {
+		for (j = 0; j < QMEMPOOL_BULK; j++) {
+			elems[j] =
+				kmem_cache_alloc(pool->kmem, gfp_mask);
+			// Handle if slab gives us NULL elem!
+			if (elems[j] == NULL) {
+				pr_err("%s() ARGH - slab returned NULL",
+				       __func__);
+				res = ring_queue_mp_enqueue_bulk(
+					pool->sharedq, elems, j-1);
+				BUG_ON(res < 0); //FIXME handle
+				return elem;
+			}
+		}
+		res = ring_queue_mp_enqueue_bulk(
+			pool->sharedq, elems, QMEMPOOL_BULK);
+		/* There is a theoretical chance that multiple
+		 * CPU enter here, refilling sharedq at the
+		 * same time, thus we must handle "full" situation
+		 */
+		BUG_ON(res < 0); /* sharedq should have room */
+	}
+
+	/* What about refilling localq ???  (guess it will happen on
+	 * next cycle, but it will cost an extra cmpxchg).  One
+	 * additional small cost for refilling localq here, would be
+	 * that we have to call this_cpu_ptr() again as the CPU could
+	 * have changed.
+	 */
+
+	return elem;
+	//TODO: Add stat counter for this event
+}
+EXPORT_SYMBOL(__qmempool_alloc_from_slab);
+
+bool __qmempool_free_to_slab(struct qmempool *pool, void **elems)
+{
+	int res, i, j;
+	/* Called when sharedq is full, thus make room */
+
+	/* free these elements for real */
+	for (i = 0; i < QMEMPOOL_BULK; i++) {
+		kmem_cache_free(pool->kmem, elems[i]);
+	}
+
+        //Q: is it wise to dealloc 48 elems to slab in one go?
+
+	/* make enough room in sharedq for next round */
+	for (i = 0; i < QMEMPOOL_REFILL_MULTIPLIER; i++) {
+		res = ring_queue_mc_dequeue_bulk(
+			pool->sharedq, elems, QMEMPOOL_BULK);
+		BUG_ON(res < 0); /* could race, but sharedq should be full */
+		for (j = 0; j < QMEMPOOL_BULK; j++) {
+			kmem_cache_free(pool->kmem, elems[j]);
+		}
+	}
+	return true;
+}
+EXPORT_SYMBOL(__qmempool_free_to_slab);
+
+
+MODULE_DESCRIPTION("Quick queue based mempool (qmempool)");
+MODULE_AUTHOR("Jesper Dangaard Brouer <netoptimizer@brouer.com>");
+MODULE_LICENSE("GPL");
