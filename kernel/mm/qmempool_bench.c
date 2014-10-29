279020c1a3da1f31b14ca6314746b280c1066b75
Jesper Dangaard Brouer <brouer@redhat.com>
2014-10-29 13:31:55 +0100

    qmempool_bench: add module for micro benchmarking qmempool
    First results.
    
    Compared against SLUB, with .config:
     CONFIG_SLUB=y
     CONFIG_SLUB_CPU_PARTIAL=y
     CONFIG_PREEMPT_COUNT=y
     CONFIG_PREEMPT=n
    
    Results from CPU E5-2695:
    
    In the fast reuse case, the SLUB cost is 19.313 ns, while the
    qmempool cost is only 9.267 ns (with only using preempt_disable).
    
    qmempool_bench: Measured cost of doing alloc+free:
    
    Type:kmem fastpath reuse Per elem: 48 cycles(tsc) 19.313 ns (step:0)
     - (measurement period time:0.579413741 sec time_interval:579413741)
     - (invoke count:30000000 tsc_interval:1448533222)
    
    Type:qmempool fastpath reuse Per elem: 23 cycles(tsc) 9.267 ns (step:0)
     - (measurement period time:0.278013891 sec time_interval:278013891)
     - (invoke count:30000000 tsc_interval:695033972)
    
    
    The interesting comparison is the usage pattern of allocating 256
    elements before freeing them again.  The qmempool only have 32
    local CPU elements, thus it must go through the sharedq to get
    enough resources, but have enough shared 256 elements to avoid
    calling the real slab.
    
    The cost of SLUB increased to 37.570 ns, while the qmempool cost
    only increased to 12.539 ns.  This shows qmempool have
    successfully amortized the cost of the expensive locked cmpxchg.
    
    qmempool_bench: N-pattern with 256 elements:
    
    Type:kmem alloc+free N-pattern Per elem: 93 cycles(tsc) 37.570 ns (step:0)
     - (measurement period time:0.961808098 sec time_interval:961808098)
     - (invoke count:25600000 tsc_interval:2404518787)
    
    Type:qmempool alloc+free N-pattern Per elem: 31 cycles(tsc) 12.539 ns (step:0)
     - (measurement period time:0.321023810 sec time_interval:321023810)
     - (invoke count:25600000 tsc_interval:802558657)
    
    If we need to use more expensive preemptive avoidance technique,
    like local_bh_{disable,enable} or local_irq_{save,restore}, then
    the qmempool cost goes up, but still maintains better performance
    than SLUB.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

---
+++ kernel/mm/qmempool_bench.c
@@ -0,0 +1,275 @@
+/*
+ * Micro-Benchmarking module for linux/qmempool.h usage
+ */
+#define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
+
+#include <linux/ring_queue.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/time_bench.h>
+#include <linux/skbuff.h>
+
+#include <linux/qmempool.h>
+
+static int verbose=1;
+
+static void print_qstats(struct qmempool *pool,
+			 const char *func, const char *msg)
+{
+	struct qmempool_percpu *cpu;
+	int localq_sz, sharedq_sz;
+
+	preempt_disable();
+	cpu = this_cpu_ptr(pool->percpu);
+	localq_sz  = ring_queue_count(cpu->localq);
+	sharedq_sz = ring_queue_count(pool->sharedq);
+	if (verbose >= 2)
+		pr_info("%s() qstats localq:%d sharedq:%d (%s)\n", func,
+			localq_sz, sharedq_sz, msg);
+	preempt_enable();
+}
+
+/*** Benchmark code execution time tests ***/
+
+struct my_elem {
+	/* element used for benchmark testing */
+	struct sk_buff skb;
+};
+
+/* For comparison benchmark against the fastpath of the
+ * slab/kmem_cache allocator
+ */
+static int benchmark_kmem_cache_fastpath_reuse(
+	struct time_bench_record *rec, void *data)
+{
+	uint64_t loops_cnt = 0;
+	int i;
+	struct my_elem *elem;
+	struct kmem_cache *slab;
+
+	slab = kmem_cache_create("qmempool_test4", sizeof(struct my_elem),
+				 0, SLAB_HWCACHE_ALIGN, NULL);
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+
+		/* request new elem */
+		elem = kmem_cache_alloc(slab, GFP_ATOMIC);
+		if (elem == NULL)
+			goto out;
+
+		barrier(); /* compiler barrier */
+
+		/* return elem */
+		kmem_cache_free(slab, elem);
+		loops_cnt++;
+	}
+out:
+	time_bench_stop(rec, loops_cnt);
+	/* cleanup */
+	kmem_cache_destroy(slab);
+	return loops_cnt;
+}
+
+static int benchmark_qmempool_fastpath_reuse(
+	struct time_bench_record *rec, void *data)
+{
+	uint64_t loops_cnt = 0;
+	int i;
+	struct my_elem *elem, *elem2;
+	struct kmem_cache *slab;
+	struct qmempool *pool;
+
+	slab = kmem_cache_create("qmempool_test4", sizeof(struct my_elem),
+				 0, SLAB_HWCACHE_ALIGN, NULL);
+
+	pool = qmempool_create(32, 128, 16, slab, GFP_ATOMIC);
+	if (pool == NULL) {
+		kmem_cache_destroy(slab);
+		return false;
+	}
+
+	// "warm-up"
+	elem  = qmempool_alloc(pool, GFP_ATOMIC);
+	elem2 = qmempool_alloc(pool, GFP_ATOMIC);
+	qmempool_free(pool, elem);
+	qmempool_free(pool, elem2);
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+
+		/* request new elem */
+		elem = qmempool_alloc(pool, GFP_ATOMIC);
+		if (elem == NULL)
+			goto out;
+
+		barrier(); /* compiler barrier */
+
+		/* return elem */
+		qmempool_free(pool, elem);
+		loops_cnt++;
+	}
+out:
+	time_bench_stop(rec, loops_cnt);
+	/* cleanup */
+	qmempool_destroy(pool);
+	kmem_cache_destroy(slab);
+	return loops_cnt;
+}
+
+/* Keeping elements in a simple array to avoid too much interference
+ * with test */
+//#define ARRAY_MAX_ELEMS 128
+#define ARRAY_MAX_ELEMS 256
+//#define ARRAY_MAX_ELEMS 256+128
+//#define ARRAY_MAX_ELEMS 1024
+struct my_elem *elems[ARRAY_MAX_ELEMS];
+
+static int benchmark_kmem_cache_pattern(
+	struct time_bench_record *rec, void *data)
+{
+	uint64_t loops_cnt = 0;
+	int i, n;
+	//size_t elem_sz = sizeof(*elems[0]); // == 232 bytes
+	struct kmem_cache *slab;
+
+	slab = kmem_cache_create("qmempool_test", sizeof(*elems[0]),
+				 0, SLAB_HWCACHE_ALIGN, NULL);
+	if (!slab)
+		return 0;
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+
+		/* alloc N new elems */
+		for (n = 0; n < ARRAY_MAX_ELEMS; n++) {
+			elems[n] = kmem_cache_alloc(slab, GFP_ATOMIC);
+		}
+
+		barrier(); /* compiler barrier */
+
+		/* free N elems */
+		for (n = 0; n < ARRAY_MAX_ELEMS; n++) {
+			kmem_cache_free(slab, elems[n]);
+			loops_cnt++;
+		}
+	}
+	time_bench_stop(rec, loops_cnt);
+
+	/* cleanup */
+	kmem_cache_destroy(slab);
+	return loops_cnt;
+}
+
+
+static int benchmark_qmempool_pattern(
+	struct time_bench_record *rec, void *data)
+{
+	uint64_t loops_cnt = 0;
+	int i, n;
+	struct kmem_cache *slab;
+	struct qmempool *pool;
+
+	slab = kmem_cache_create("qmempool_test", sizeof(*elems[0]),
+				 0, SLAB_HWCACHE_ALIGN, NULL);
+	if (!slab)
+		return 0;
+	//pool = qmempool_create(32, 128, 0, slab, GFP_ATOMIC);
+	pool = qmempool_create(32, 256, 0, slab, GFP_ATOMIC);
+	//pool = qmempool_create(32, 1024, 0, slab, GFP_ATOMIC);
+	if (pool == NULL) {
+		kmem_cache_destroy(slab);
+		return 0;
+	}
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+
+		/* alloc N new elems */
+		for (n = 0; n < ARRAY_MAX_ELEMS; n++) {
+			elems[n] = qmempool_alloc(pool, GFP_ATOMIC);
+		}
+
+		barrier(); /* compiler barrier */
+
+		/* free N elems */
+		for (n = 0; n < ARRAY_MAX_ELEMS; n++) {
+			qmempool_free(pool, elems[n]);
+			loops_cnt++;
+		}
+	}
+	time_bench_stop(rec, loops_cnt);
+
+	print_qstats(pool, __func__, "ZZZ");
+
+	/* cleanup */
+	qmempool_destroy(pool);
+	kmem_cache_destroy(slab);
+	return loops_cnt;
+}
+
+bool run_micro_benchmark_tests(void)
+{
+	uint32_t loops = 1000000;
+
+	/* Results listed below for a E5-2695 CPU */
+	pr_info("Measured cost of doing alloc+free:\n");
+
+	/* Results:
+	 *  19.313 ns with SLUB
+	 */
+	time_bench_loop(loops*30, 0, "kmem fastpath reuse", NULL,
+			benchmark_kmem_cache_fastpath_reuse);
+	/* Results:
+	 *   9.267 ns with preempt_{disable/enable}
+	 *  18.891 ns with local_bh_{disable/enable}
+	 */
+	time_bench_loop(loops*30, 0, "qmempool fastpath reuse", NULL,
+			benchmark_qmempool_fastpath_reuse);
+
+	pr_info("N-pattern with %d elements\n", ARRAY_MAX_ELEMS);
+
+	/* Results:
+	 *  37.570 ns N=256 with SLUB
+	 */
+	time_bench_loop(loops/10, 0, "kmem alloc+free N-pattern", NULL,
+			benchmark_kmem_cache_pattern);
+	/* Results:
+	 *  12.539 ns N=256 with preempt_{disable/enable}
+	 *  23.409 ns N=256 with local_bh_{disable/enable}
+	 */
+	time_bench_loop(loops/10, 0, "qmempool alloc+free N-pattern", NULL,
+			benchmark_qmempool_pattern);
+
+	return true;
+}
+
+static int __init qmempool_bench_module_init(void)
+{
+	preempt_disable();
+	pr_info("DEBUG: cpu:%d\n", smp_processor_id());
+	preempt_enable();
+
+	if (verbose)
+		pr_info("Loaded\n");
+
+	run_micro_benchmark_tests();
+
+	return 0;
+}
+module_init(qmempool_bench_module_init);
+
+static void __exit qmempool_bench_module_exit(void)
+{
+	// TODO: perform sanity checks, and free mem
+	if (verbose)
+		pr_info("Unloaded\n");
+}
+module_exit(qmempool_bench_module_exit);
+
+MODULE_DESCRIPTION("Micro Benchmarking of qmempool");
+MODULE_AUTHOR("Jesper Dangaard Brouer <netoptimizer@brouer.com>");
+MODULE_LICENSE("GPL");
