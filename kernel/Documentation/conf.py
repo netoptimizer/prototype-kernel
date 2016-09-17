6fcc6a0e4fdd6811542aea68cef806b2cccafdb4
Jesper Dangaard Brouer <brouer@redhat.com>
2016-09-17 15:37:21 +0200

    docs: remove sphinx extensions autodoc and githubpages
    ReadTheDocs does not seem to like the githubpages extension.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- kernel/Documentation/conf.py
+++ kernel/Documentation/conf.py
@@ -30,11 +30,12 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+#     'sphinx.ext.autodoc',
+#     'sphinx.ext.githubpages',
+#
 extensions = [
-    'sphinx.ext.autodoc',
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
-    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
