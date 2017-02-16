5e09eb811abfbecc268bcf7d435580d314a1f330
Jesper Dangaard Brouer <brouer@redhat.com>
2017-02-16 15:17:46 +0100

    samples/bpf: xdp_ddos01 cmdline add options for --add and --del
    Makes it possible to add and remove src IPs from the blacklist

--- kernel/samples/bpf/xdp_ddos01_blacklist_cmdline.c
+++ kernel/samples/bpf/xdp_ddos01_blacklist_cmdline.c
@@ -26,8 +26,10 @@ static const char *__doc__=
 #include "xdp_ddos01_blacklist_common.h"
 
 static const struct option long_options[] = {
-	{"help",	no_argument,			NULL, 'h' },
-	{"ip",		required_argument,		NULL, 's' },
+	{"help",	no_argument,		NULL, 'h' },
+	{"add",		no_argument,		NULL, 'a' },
+	{"del",		no_argument,		NULL, 'd' },
+	{"ip",		required_argument,	NULL, 's' },
 	{0, 0, NULL,  0 }
 };
 
@@ -67,11 +69,17 @@ int open_bpf_map(const char *file)
 
 #define STR_MAX 42
 
+#define ACTION_ADD (1 << 0)
+#define ACTION_DEL (1 << 1)
+
 int main(int argc, char **argv)
 {
 	int longindex = 0;
 	int fd_blacklist;
 	int fd_verdict;
+	unsigned int action = 0;
+	char _ip_string_buf[STR_MAX] = {};
+	char *ip_string = NULL;
 	int opt;
 
 	fd_blacklist = open_bpf_map(file_blacklist);
@@ -80,13 +88,20 @@ int main(int argc, char **argv)
 	while ((opt = getopt_long(argc, argv, "hs:",
 				  long_options, &longindex)) != -1) {
 		switch (opt) {
+		case 'a':
+			action |= ACTION_ADD;
+			break;
+		case 'd':
+			action |= ACTION_DEL;
+			break;
 		case 's':
-			printf("Blacklist add IP:%s\n", optarg);
+			printf("Blacklist IP:%s\n", optarg);
 			if (!optarg || strlen(optarg) >= STR_MAX) {
-				printf("ERR: srcip too long or NULL\n");
+				printf("ERR: src ip too long or NULL\n");
 				goto error;
 			}
-			blacklist_add(fd_blacklist, optarg);
+			ip_string = (char *)&_ip_string_buf;
+			strncpy(ip_string, optarg, STR_MAX);
 			break;
 		case 'h':
 		error:
@@ -96,4 +111,18 @@ int main(int argc, char **argv)
 		}
 	}
 
+	/* Update blacklist */
+	if (ip_string) {
+		if ((action == ACTION_ADD))
+			blacklist_add(fd_blacklist, ip_string);
+		else if ((action == ACTION_DEL) && ip_string)
+			//blacklist_add(fd_blacklist, ip_string);
+			;
+		else {
+			printf("Warning: not action specified for IP:%s\n",
+				ip_string);
+		}
+	}
+
+	/* TODO: Implement stats */
 }
