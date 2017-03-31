4c3a0e4b7ae111726f7ccca774d7652688cff7c7
Jesper Dangaard Brouer <brouer@redhat.com>
2017-03-31 18:47:00 +0200

    xdp_bench02: add code that parse packet to increase code size
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- kernel/samples/bpf/xdp_bench02_drop_pattern_kern.c
+++ kernel/samples/bpf/xdp_bench02_drop_pattern_kern.c
@@ -1,8 +1,19 @@
 /* xdp_bench02_drop_pattern */
 #include <uapi/linux/bpf.h>
 #include <uapi/linux/if_ether.h>
+#include <uapi/linux/if_packet.h>
+#include <uapi/linux/if_vlan.h>
+#include <uapi/linux/ip.h>
 #include <uapi/linux/in.h>
+#include <uapi/linux/tcp.h>
+#include <uapi/linux/udp.h>
+
 #include "bpf_helpers.h"
+
+struct vlan_hdr {
+	__be16 h_vlan_TCI;
+	__be16 h_vlan_encapsulated_proto;
+};
 
 struct pattern {
 	/* Remember: sync with _user.c */
@@ -68,6 +79,14 @@ void stats_action_verdict(u32 action)
 		*value += 1;
 }
 
+struct bpf_map_def SEC("maps") blacklist = {
+	.type        = BPF_MAP_TYPE_PERCPU_HASH,
+	.key_size    = sizeof(u32),
+	.value_size  = sizeof(u64), /* Drop counter */
+	.max_entries = 100000,
+	.map_flags   = BPF_F_NO_PREALLOC,
+};
+
 /*
  * Pattern1: N-drop + N-accept
  *
@@ -103,6 +122,85 @@ u32 N_drop_N_accept(u64 N)
 }
 
 
+static __always_inline
+u32 parse_ipv4(struct xdp_md *ctx, u64 l3_offset)
+{
+	void *data_end = (void *)(long)ctx->data_end;
+	void *data     = (void *)(long)ctx->data;
+	struct iphdr *iph = data + l3_offset;
+	u64 *value;
+	u32 ip_src; /* type need to match map */
+
+	/* Hint: +1 is sizeof(struct iphdr) */
+	if (iph + 1 > data_end)
+		return XDP_ABORTED;
+
+	ip_src = iph->saddr; /* Extract key */
+
+	value = bpf_map_lookup_elem(&blacklist, &ip_src);
+	if (value) {
+		/* Don't need __sync_fetch_and_add(); as percpu map */
+		*value += 1; /* Keep a counter for drop matches */
+		return XDP_DROP;
+	}
+
+	return XDP_PASS;
+}
+
+/* Parse Ethernet layer 2, extract network layer 3 offset and protocol
+ *
+ * Returns false on error and non-supported ether-type
+ */
+static __always_inline
+bool parse_eth(struct ethhdr *eth, void *data_end,
+	       u16 *eth_proto, u64 *l3_offset)
+{
+	u16 eth_type;
+	u64 offset;
+
+	offset = sizeof(*eth);
+	if ((void *)eth + offset > data_end)
+		return false;
+
+	eth_type = eth->h_proto;
+
+	/* Skip non 802.3 Ethertypes */
+	if (unlikely(ntohs(eth_type) < ETH_P_802_3_MIN))
+		return false;
+
+	/* Handle VLAN tagged packet */
+	if (eth_type == htons(ETH_P_8021Q) || eth_type == htons(ETH_P_8021AD)) {
+		struct vlan_hdr *vlan_hdr;
+
+		vlan_hdr = (void *)eth + offset;
+		offset += sizeof(*vlan_hdr);
+		if ((void *)eth + offset > data_end)
+			return false;
+		eth_type = vlan_hdr->h_vlan_encapsulated_proto;
+	}
+	/* TODO: Handle double VLAN tagged packet */
+
+	*eth_proto = ntohs(eth_type);
+	*l3_offset = offset;
+	return true;
+}
+
+static __always_inline
+u32 handle_eth_protocol(struct xdp_md *ctx, u16 eth_proto, u64 l3_offset)
+{
+	switch (eth_proto) {
+	case ETH_P_IP:
+		return parse_ipv4(ctx, l3_offset);
+		break;
+	case ETH_P_IPV6: /* Not handler for IPv6 yet*/
+	case ETH_P_ARP:  /* Let OS handle ARP */
+		/* Fall-through */
+	default:
+		return XDP_PASS; /* Not handling eth_proto */
+	}
+	return XDP_PASS;
+}
+
 SEC("xdp_bench02")
 int xdp_prog(struct xdp_md *ctx)
 {
@@ -113,6 +211,7 @@ int xdp_prog(struct xdp_md *ctx)
 	long *value;
 	u64 offset;
 	u32 action = XDP_DROP;
+	u32 action2;
 	struct pattern *pattern;
 	u32 key = 0;
 	u64 *touch_mem;
@@ -131,11 +230,16 @@ int xdp_prog(struct xdp_md *ctx)
 	touch_mem = bpf_map_lookup_elem(&touch_memory, &key);
 	if (touch_mem && (*touch_mem == 1)) {
 		struct ethhdr *eth = data;
+		u16 eth_proto = 0;
+		u64 l3_offset = 0;
 
-		eth_type = eth->h_proto;
-		/* Avoid compile removing this: e.g Drop non 802.3 Ethertypes */
-		if (ntohs(eth_type) < ETH_P_802_3_MIN)
-			return XDP_DROP;
+		if (!(parse_eth(eth, data_end, &eth_proto, &l3_offset)))
+			return XDP_PASS; /* Skip */
+		action2 = handle_eth_protocol(ctx, eth_proto, l3_offset);
+		if (action2 == XDP_DROP) {
+			stats_action_verdict(action);
+			return action2;
+		}
 	}
 
 	value = bpf_map_lookup_elem(&rx_cnt, &key);
@@ -144,6 +248,8 @@ int xdp_prog(struct xdp_md *ctx)
 
 	if (pattern->type == 1)
 		action = N_drop_N_accept(pattern->arg);
+
+	// TODO: add "always" XDP_DROP to measure baseline cost of program
 
 	stats_action_verdict(action);
 	return action;
