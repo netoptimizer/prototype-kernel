403fb16b93430ca9225be30902b21ef573417688
Jesper Dangaard Brouer <brouer@redhat.com>
2017-05-16 16:39:47 +0200

    bpf: add bpf_debug helper in xdp_rxhash
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- kernel/samples/bpf/xdp_rxhash_kern.c
+++ kernel/samples/bpf/xdp_rxhash_kern.c
@@ -10,6 +10,21 @@
 
 #include "bpf_helpers.h"
 
+#define DEBUG 1
+#ifdef  DEBUG
+/* Only use this for debug output. Notice output from bpf_trace_printk()
+ * end-up in /sys/kernel/debug/tracing/trace_pipe (remember use cat)
+ */
+# define bpf_debug(fmt, ...)                                            \
+	({							       \
+		char ____fmt[] = fmt;				       \
+		bpf_trace_printk(____fmt, sizeof(____fmt),	       \
+				 ##__VA_ARGS__);		       \
+	})
+#else
+# define bpf_debug(fmt, ...) { } while (0)
+#endif
+
 struct bpf_map_def SEC("maps") rx_cnt = {
 	.type = BPF_MAP_TYPE_PERCPU_ARRAY,
 	.key_size = sizeof(u32),
