438aed966dc325a09b69c1525aab9f7dea8bed9a
Jesper Dangaard Brouer <brouer@redhat.com>
2020-01-15 12:44:05 +0100

    bench_page_pool_simple: initial use of time_bench infra
    Added some baseline test, whcih is useful to get an understand
    of the baseline overhead of operations on this hardware.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- kernel/lib/bench_page_pool_simple.c
+++ kernel/lib/bench_page_pool_simple.c
@@ -5,15 +5,96 @@
 
 #include <linux/module.h>
 #include <linux/time_bench.h>
+#include <net/page_pool.h>
 
 static int verbose=1;
+
+/* Timing at the nanosec level, we need to know the overhead
+ * introduced by the for loop itself */
+static int time_bench_for_loop(
+	struct time_bench_record *rec, void *data)
+{
+	uint64_t loops_cnt = 0;
+	int i;
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+		loops_cnt++;
+		barrier(); /* avoid compiler to optimize this loop */
+	}
+	time_bench_stop(rec, loops_cnt);
+	return loops_cnt;
+}
+
+static int time_bench_atomic_inc(
+	struct time_bench_record *rec, void *data)
+{
+	uint64_t loops_cnt = 0;
+	atomic_t cnt;
+	int i;
+
+	atomic_set(&cnt, 0);
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+		atomic_inc(&cnt);
+		barrier(); /* avoid compiler to optimize this loop */
+	}
+	loops_cnt = atomic_read(&cnt);
+	time_bench_stop(rec, loops_cnt);
+	return loops_cnt;
+}
+
+static int time_bench_lock(
+	struct time_bench_record *rec, void *data)
+{
+	uint64_t loops_cnt = 0;
+	spinlock_t lock;
+	int i;
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+		spin_lock(&lock);
+		loops_cnt++;
+		barrier(); /* avoid compiler to optimize this loop */
+		spin_unlock(&lock);
+	}
+	time_bench_stop(rec, loops_cnt);
+	return loops_cnt;
+}
+
+int run_benchmark_tests(void)
+{
+	uint32_t loops = 10000000;
+	int passed_count = 0;
+
+	/* Baseline tests */
+	time_bench_loop(loops*10, 0,
+			"for_loop", NULL, time_bench_for_loop);
+	time_bench_loop(loops*10, 0,
+			"atomic_inc", NULL, time_bench_atomic_inc);
+	time_bench_loop(loops, 0,
+			"lock", NULL, time_bench_lock);
+
+
+
+	return passed_count;
+}
 
 static int __init bench_page_pool_simple_module_init(void)
 {
 	if (verbose)
 		pr_info("Loaded\n");
 
-	return 0;
+	if (run_benchmark_tests() < 0) {
+		return -ECANCELED;
+	}
+
+	//return 0;
+	return -EAGAIN; // Trick to not fully load module
 }
 module_init(bench_page_pool_simple_module_init);
 
