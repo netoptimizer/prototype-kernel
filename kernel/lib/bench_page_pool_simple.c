0bdc28b54279cc553afcd052d42c6c3d972bfced
Jesper Dangaard Brouer <brouer@redhat.com>
2020-01-16 13:49:38 +0100

    bench_page_pool_simple: playing with ptr_ring testing
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- kernel/lib/bench_page_pool_simple.c
+++ kernel/lib/bench_page_pool_simple.c
@@ -68,6 +68,25 @@ static int time_bench_lock(
 	return loops_cnt;
 }
 
+/* Helper for filling some page's into ptr_ring */
+static void pp_fill_ptr_ring(struct page_pool *pp, int elems)
+{
+	gfp_t gfp_mask = GFP_ATOMIC; /* GFP_ATOMIC needed when under run softirq */
+	struct page **array;
+	int i;
+
+	array = kzalloc(sizeof(struct page*) * elems, gfp_mask);
+
+	for (i = 0; i < elems; i++) {
+		array[i] = page_pool_alloc_pages(pp, gfp_mask);
+	}
+	for (i = 0; i < elems; i++) {
+		page_pool_put_page(pp, array[i], false);
+	}
+
+	kfree(array);
+}
+
 static int time_bench_page_pool01(
 	struct time_bench_record *rec, void *data)
 {
@@ -93,13 +112,14 @@ static int time_bench_page_pool01(
 		pr_warn("%s: Error(%d) creating page_pool\n", __func__, err);
 		goto out;
 	}
+	pp_fill_ptr_ring(pp, 64);
 
-	local_bh_disable();
+//	local_bh_disable();
 
 	if (in_serving_softirq())
-		pr_warn("in_serving_softirq\n");
+		pr_warn("%s(): in_serving_softirq\n", __func__);
 	else
-		pr_warn("Cannot use page_pool fast-path\n");
+		pr_warn("%s(): Cannot use page_pool fast-path\n", __func__);
 
 	time_bench_start(rec);
 	/** Loop to measure **/
@@ -116,11 +136,74 @@ static int time_bench_page_pool01(
 		page_pool_recycle_direct(pp, page);
 	}
 	time_bench_stop(rec, loops_cnt);
-	local_bh_enable();
+//	local_bh_enable();
 out:
 	page_pool_destroy(pp);
 	return loops_cnt;
 }
+
+static int time_bench_page_pool02(
+	struct time_bench_record *rec, void *data)
+{
+	uint64_t loops_cnt = 0;
+	gfp_t gfp_mask = GFP_ATOMIC; /* GFP_ATOMIC is not really needed */
+	int i, err;
+
+	struct page_pool *pp;
+	struct page *page;
+
+	struct page_pool_params pp_params = {
+		.order = 0,
+		.flags = 0,
+		.pool_size = 1024,
+		.nid = NUMA_NO_NODE,
+		.dev = NULL, /* Only use for DMA mapping */
+		.dma_dir = DMA_BIDIRECTIONAL,
+	};
+
+	pp = page_pool_create(&pp_params);
+	if (IS_ERR(pp)) {
+		err = PTR_ERR(pp);
+		pr_warn("%s: Error(%d) creating page_pool\n", __func__, err);
+		goto out;
+	}
+
+//	local_bh_disable();
+
+	if (in_serving_softirq())
+		pr_warn("%s(): in_serving_softirq\n", __func__);
+	else
+		pr_warn("Cannot use page_pool fast-path\n");
+
+/*
+  Q: Will the cached pages only be 1 page?
+  Q: Will that hurt performance when going through ptr_ring?
+*/
+
+	/* Q: Will performance improve if we create some more pages in ptr_ring? */
+	pp_fill_ptr_ring(pp, 64);
+
+	time_bench_start(rec);
+	/** Loop to measure **/
+	for (i = 0; i < rec->loops; i++) {
+		page = page_pool_alloc_pages(pp, gfp_mask);
+		if (!page)
+			break;
+		loops_cnt++;
+		barrier(); /* avoid compiler to optimize this loop */
+
+		/* Issue: this module is in_serving_softirq() and thus
+		 * cannot test the fast-path return.
+		 */
+		page_pool_put_page(pp, page, false);
+	}
+	time_bench_stop(rec, loops_cnt);
+//	local_bh_enable();
+out:
+	page_pool_destroy(pp);
+	return loops_cnt;
+}
+
 
 /* Testing page_pool requires running under softirq.
  *
@@ -141,10 +224,11 @@ static void pp_tasklet_handler(unsigned long data)
 	time_bench_loop(loops, 0,
 			"tasklet_page_pool01", NULL, time_bench_page_pool01);
 
+	time_bench_loop(loops, 0,
+			"tasklet_page_pool02", NULL, time_bench_page_pool02);
+
 }
 DECLARE_TASKLET(pp_tasklet, pp_tasklet_handler, 0);
-
-
 
 int run_benchmark_tests(void)
 {
