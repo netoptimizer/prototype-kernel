7171ad5350ea6e196097b8752221f3e574cd0b5a
Jesper Dangaard Brouer <brouer@redhat.com>
2020-01-17 13:28:35 +0100

    bench_page_pool_simple: Allow changing loops as module param
    This is practical when doing perf record to get enought data.
    
    Signed-off-by: Jesper Dangaard Brouer <brouer@redhat.com>

--- kernel/lib/bench_page_pool_simple.c
+++ kernel/lib/bench_page_pool_simple.c
@@ -8,8 +8,10 @@
 #include <net/page_pool.h>
 
 #include <linux/interrupt.h>
+#include <linux/limits.h>
 
 static int verbose=1;
+#define MY_POOL_SIZE	1024
 
 /* Makes tests selectable. Useful for perf-record to analyze a single test.
  * Hint: Bash shells support writing binary number like: $((2#101010)
@@ -32,7 +34,10 @@ enum benchmark_bit {
 #define bit(b)		(1 << (b))
 #define enabled(b)	((run_flags & (bit(b))))
 
-#define MY_POOL_SIZE	1024
+/* notice time_bench is limited to U32_MAX nr loops */
+static unsigned long loops = 10000000;
+module_param(loops, ulong, 0);
+MODULE_PARM_DESC(loops, "Specify loops bench will run");
 
 /* Timing at the nanosec level, we need to know the overhead
  * introduced by the for loop itself */
@@ -279,7 +284,7 @@ out:
  */
 static void pp_tasklet_handler(unsigned long data)
 {
-	uint32_t loops = 1000000;
+	uint32_t nr_loops = loops;
 
 	if (in_serving_softirq())
 		pr_warn("%s(): in_serving_softirq fast-path\n", __func__); // True
@@ -287,15 +292,15 @@ static void pp_tasklet_handler(unsigned long data)
 		pr_warn("%s(): Cannot use page_pool fast-path\n", __func__);
 
 	if (enabled(bit_run_bench_tasklet01))
-		time_bench_loop(loops, 0,
+		time_bench_loop(nr_loops, 0,
 				"tasklet_page_pool01", NULL, time_bench_page_pool01);
 
 	if (enabled(bit_run_bench_tasklet02))
-		time_bench_loop(loops, 0,
+		time_bench_loop(nr_loops, 0,
 				"tasklet_page_pool02", NULL, time_bench_page_pool02);
 
 	if (enabled(bit_run_bench_tasklet03))
-		time_bench_loop(loops, 0,
+		time_bench_loop(nr_loops, 0,
 				"tasklet_page_pool03", NULL, time_bench_page_pool03);
 
 }
@@ -310,28 +315,28 @@ static void run_tasklet_tests(void)
 
 static int run_benchmark_tests(void)
 {
-	uint32_t loops = 10000000;
+	uint32_t nr_loops = loops;
 	int passed_count = 0;
 
 	/* Baseline tests */
 	if (enabled(bit_run_bench_baseline)) {
-		time_bench_loop(loops*10, 0,
+		time_bench_loop(nr_loops*10, 0,
 				"for_loop", NULL, time_bench_for_loop);
-		time_bench_loop(loops*10, 0,
+		time_bench_loop(nr_loops*10, 0,
 				"atomic_inc", NULL, time_bench_atomic_inc);
-		time_bench_loop(loops, 0,
+		time_bench_loop(nr_loops, 0,
 				"lock", NULL, time_bench_lock);
 	}
 
 	/* This test cannot activate correct code path, due to no-softirq ctx */
 	if (enabled(bit_run_bench_no_softirq01))
-		time_bench_loop(loops, 0,
+		time_bench_loop(nr_loops, 0,
 				"no-softirq-page_pool01", NULL, time_bench_page_pool01);
 	if (enabled(bit_run_bench_no_softirq02))
-		time_bench_loop(loops, 0,
+		time_bench_loop(nr_loops, 0,
 				"no-softirq-page_pool02", NULL, time_bench_page_pool02);
 	if (enabled(bit_run_bench_no_softirq03))
-		time_bench_loop(loops, 0,
+		time_bench_loop(nr_loops, 0,
 				"no-softirq-page_pool03", NULL, time_bench_page_pool03);
 
 	return passed_count;
@@ -342,11 +347,20 @@ static int __init bench_page_pool_simple_module_init(void)
 	if (verbose)
 		pr_info("Loaded\n");
 
+	if (loops > U32_MAX) {
+		pr_err("Module param loops(%lu) exceeded U32_MAX(%u)\n",
+		       loops, U32_MAX);
+		return -ECHRNG;
+	}
+
 	run_benchmark_tests();
 	run_tasklet_tests();
 
+	/* Killing tasklet force us wait until it is done. Problem is it will be
+	 * spinning while waiting for the tasklet to complete.
+	 */
+	tasklet_kill(&pp_tasklet);
 	return 0;
-	// tasklet_kill(&pp_tasklet);
 	// return -EAGAIN; // Trick to not fully load module
 }
 module_init(bench_page_pool_simple_module_init);
