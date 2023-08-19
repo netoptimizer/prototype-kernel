7fb2a9cf987fb1da922c949075bc3c8bbe98ef3e
Jesper Dangaard Brouer <hawk@kernel.org>
2023-08-19 10:41:15 +0200

    Fix page_pool: newer kernel unexported page_pool_release_page()
    Bump refcnt on page before using PP put_page API, which cause
    it to not get recycles, and then put_page release it to
    free it to page allocator.
    
    This extra refcnt of-cause will affect the benchmark, but
    users should just take this into account.
    
    Fixes: 535b9c61bdef ("net: page_pool: hide page_pool_release_page()")
    Signed-off-by: Jesper Dangaard Brouer <hawk@kernel.org>

--- kernel/lib/bench_page_pool_simple.c
+++ kernel/lib/bench_page_pool_simple.c
@@ -211,8 +211,14 @@ int time_bench_page_pool(
 			/* Test if not pages are recycled, but instead
 			 * returned back into systems page allocator
 			 */
+#if LINUX_VERSION_CODE < KERNEL_VERSION(6, 5, 0)
 			page_pool_release_page(pp, page);
 			put_page(page);
+#else
+			get_page(page); /* cause no-recycling */
+			_page_pool_put_page(pp, page, false);
+			put_page(page);
+#endif
 		} else {
 			BUILD_BUG();
 		}
